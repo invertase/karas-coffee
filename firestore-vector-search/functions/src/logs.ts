dea200f1f38a6eb88d61e42e68464b72f20477cc
Jacob Cable <jacobcable94@gmail.com>
2024-03-29 11:43:30 +0000

    chore: removed local extensions source

--- firestore-vector-search/functions/src/logs.ts
+++
@@ -1,6 +0,0 @@
-import { obfuscatedConfig } from "./config";
-import { logger } from "firebase-functions";
-
-export function init() {
-  logger.log("Initializing extension with configuration", obfuscatedConfig);
-}
