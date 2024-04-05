3b99fd6f2338dfc239f1700fd169ef3bc217b33b
Jacob Cable <jacobcable94@gmail.com>
2024-04-05 13:48:34 +0100

    fix: disable twilio for demo

--- website/src/routes/Account/Overview.tsx
+++ website/src/routes/Account/Overview.tsx
@@ -47,7 +47,7 @@ export function Overview() {
           <li>Display Name: {data.displayName}</li>
           <li>Email Address: {data.email || 'N/A'}</li>
           <li>Email Verified: {data.emailVerified ? 'Yes' : 'No'}</li>
-          <PhoneNumber user={data} />
+          {/* disabled for demo <PhoneNumber user={data} /> */}
         </ul>
         <div className="mt-4">
           {!data.isAnonymous && (
